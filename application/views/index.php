1f96c449c834a8d845ecf9b0eb6559a86310f996
sipaku <bebyrpl@gmail.com>
2021-09-08 23:22:20 +0700

    UPDATE INDEX AGAIN
    YEAH IM JUST TRY IT

--- application/views/index.php
+++ application/views/index.php
@@ -15,7 +15,7 @@
 	<!-- meta character set -->
 	<meta charset="UTF-8">
 	<!-- Site Title -->
-	<title>SIPAKU</title>
+	<title>SIPAKUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU</title>
 	<link rel="stylesheet" href="<?php echo base_url('assets/fontawesome/css/all.css') ?>">
 	<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
 	<!--CSS============================================= -->
