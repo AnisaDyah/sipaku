99fc1454636dc3670eeef23477eb2aff5c57d379
sipaku <bebyrpl@gmail.com>
2021-09-08 23:20:09 +0700

    UPDATE INDEX.PHP
    IM JUST UPATE TITLE

--- application/views/index.php
+++ application/views/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="zxx" class="no-js">
+
 <head>
 	<!-- Mobile Specific Meta -->
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -14,7 +15,7 @@
 	<!-- meta character set -->
 	<meta charset="UTF-8">
 	<!-- Site Title -->
-	<title>SIPAKO</title>
+	<title>SIPAKU</title>
 	<link rel="stylesheet" href="<?php echo base_url('assets/fontawesome/css/all.css') ?>">
 	<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
 	<!--CSS============================================= -->
@@ -27,92 +28,109 @@
 	<link rel="stylesheet" href="<?php echo base_url('assets/css/owl.carousel.css') ?>">
 	<link rel="stylesheet" href="<?php echo base_url('assets/css/main.css') ?>">
 	<style>
-    body {
-		font-family: 'Varela Round', sans-serif;
-	}
-	.modal-login {		
-		color: #636363;
-		width: 350px;
-		margin: 80px auto 0;
-	}
-	.modal-login .modal-content {
-		padding: 20px;
-		border-radius: 5px;
-		border: none;
-	}
-	.modal-login .modal-header {
-		border-bottom: none;   
-        position: relative;
-        justify-content: center;
-	}
-	.modal-login h4 {
-		text-align: center;
-		font-size: 26px;
-		margin: 30px 0 -15px;
-	}
-	.modal-login .form-control:focus {
-		border-color: #70c5c0;
-	}
-	.modal-login .form-control, .modal-login .btn {
-		min-height: 40px;
-		border-radius: 3px; 
-	}
-	.modal-login .close {
-        position: absolute;
-		top: -5px;
-		right: -5px;
-	}	
-	.modal-login .modal-footer {
-		background: #ecf0f1;
-		border-color: #dee4e7;
-		text-align: center;
-        justify-content: center;
-		margin: 0 -20px -20px;
-		border-radius: 5px;
-		font-size: 13px;
-	}
-	.modal-login .modal-footer a {
-		color: #999;
-	}		
-	.modal-login .avatar {
-		position: absolute;
-		margin: 0 auto;
-		left: 0;
-		right: 0;
-		top: -70px;
-		width: 95px;
-		height: 95px;
-		border-radius: 50%;
-		z-index: 9;
-		background: #DEB887;
-		padding: 15px;
-		box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.1);
-	}
-	.modal-login .avatar img {
-		width: 100%;
-	}
-    .modal-login .btn {
-        color: #fff;
-        border-radius: 4px;
-		background: #8B4513;
-		text-decoration: none;
-		transition: all 0.4s;
-        line-height: normal;
-        border: none;
-    }
-	.modal-login .btn:hover, .modal-login .btn:focus {
-		background: #FFF8DC;
-		outline: none;
-	}
-	.trigger-btn {
-		display: inline-block;
-		margin: 100px auto;
-	}
+		body {
+			font-family: 'Varela Round', sans-serif;
+		}
+
+		.modal-login {
+			color: #636363;
+			width: 350px;
+			margin: 80px auto 0;
+		}
+
+		.modal-login .modal-content {
+			padding: 20px;
+			border-radius: 5px;
+			border: none;
+		}
+
+		.modal-login .modal-header {
+			border-bottom: none;
+			position: relative;
+			justify-content: center;
+		}
+
+		.modal-login h4 {
+			text-align: center;
+			font-size: 26px;
+			margin: 30px 0 -15px;
+		}
+
+		.modal-login .form-control:focus {
+			border-color: #70c5c0;
+		}
+
+		.modal-login .form-control,
+		.modal-login .btn {
+			min-height: 40px;
+			border-radius: 3px;
+		}
+
+		.modal-login .close {
+			position: absolute;
+			top: -5px;
+			right: -5px;
+		}
+
+		.modal-login .modal-footer {
+			background: #ecf0f1;
+			border-color: #dee4e7;
+			text-align: center;
+			justify-content: center;
+			margin: 0 -20px -20px;
+			border-radius: 5px;
+			font-size: 13px;
+		}
+
+		.modal-login .modal-footer a {
+			color: #999;
+		}
+
+		.modal-login .avatar {
+			position: absolute;
+			margin: 0 auto;
+			left: 0;
+			right: 0;
+			top: -70px;
+			width: 95px;
+			height: 95px;
+			border-radius: 50%;
+			z-index: 9;
+			background: #DEB887;
+			padding: 15px;
+			box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.1);
+		}
+
+		.modal-login .avatar img {
+			width: 100%;
+		}
+
+		.modal-login .btn {
+			color: #fff;
+			border-radius: 4px;
+			background: #8B4513;
+			text-decoration: none;
+			transition: all 0.4s;
+			line-height: normal;
+			border: none;
+		}
+
+		.modal-login .btn:hover,
+		.modal-login .btn:focus {
+			background: #FFF8DC;
+			outline: none;
+		}
+
+		.trigger-btn {
+			display: inline-block;
+			margin: 100px auto;
+		}
 	</style>
 </head>
+
 <body>
 	<!-- #navbar -->
-	<?php $this->load->view('header.php')?>
+	<?php $this->load->view('header.php') ?>
 	<!-- #end navbar -->
 
 	<!-- start banner header Area -->
@@ -168,23 +186,24 @@
 	<!-- End menu Area -->
 
 	<!-- start footer Area -->
-	<?php $this->load->view('footer.php')?>
+	<?php $this->load->view('footer.php') ?>
 	<!-- End footer Area -->
 
-	<script src="<?php echo base_url('assets/')?>js/vendor/jquery-2.2.4.min.js"></script>
-	<script src="<?php echo base_url('assets/')?>js/vendor/bootstrap.min.js"></script>
-	<script src="<?php echo base_url('assets/')?>js/easing.min.js"></script>
-	<script src="<?php echo base_url('assets/')?>js/hoverIntent.js"></script>
-	<script src="<?php echo base_url('assets/')?>js/superfish.min.js"></script>
-	<script src="<?php echo base_url('assets/')?>js/jquery.ajaxchimp.min.js"></script>
-	<script src="<?php echo base_url('assets/')?>js/jquery.magnific-popup.min.js"></script>
-	<script src="<?php echo base_url('assets/')?>js/owl.carousel.min.js"></script>
-	<script src="<?php echo base_url('assets/')?>js/jquery.sticky.js"></script>
-	<script src="<?php echo base_url('assets/')?>js/jquery.nice-select.min.js"></script>
-	<script src="<?php echo base_url('assets/')?>js/parallax.min.js"></script>
-	<script src="<?php echo base_url('assets/')?>js/waypoints.min.js"></script>
-	<script src="<?php echo base_url('assets/')?>js/jquery.counterup.min.js"></script>
-	<script src="<?php echo base_url('assets/')?>js/mail-script.js"></script>
-	<script src="<?php echo base_url('assets/')?>/js/main.js"></script>
+	<script src="<?php echo base_url('assets/') ?>js/vendor/jquery-2.2.4.min.js"></script>
+	<script src="<?php echo base_url('assets/') ?>js/vendor/bootstrap.min.js"></script>
+	<script src="<?php echo base_url('assets/') ?>js/easing.min.js"></script>
+	<script src="<?php echo base_url('assets/') ?>js/hoverIntent.js"></script>
+	<script src="<?php echo base_url('assets/') ?>js/superfish.min.js"></script>
+	<script src="<?php echo base_url('assets/') ?>js/jquery.ajaxchimp.min.js"></script>
+	<script src="<?php echo base_url('assets/') ?>js/jquery.magnific-popup.min.js"></script>
+	<script src="<?php echo base_url('assets/') ?>js/owl.carousel.min.js"></script>
+	<script src="<?php echo base_url('assets/') ?>js/jquery.sticky.js"></script>
+	<script src="<?php echo base_url('assets/') ?>js/jquery.nice-select.min.js"></script>
+	<script src="<?php echo base_url('assets/') ?>js/parallax.min.js"></script>
+	<script src="<?php echo base_url('assets/') ?>js/waypoints.min.js"></script>
+	<script src="<?php echo base_url('assets/') ?>js/jquery.counterup.min.js"></script>
+	<script src="<?php echo base_url('assets/') ?>js/mail-script.js"></script>
+	<script src="<?php echo base_url('assets/') ?>/js/main.js"></script>
 </body>
+
 </html>
